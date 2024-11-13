b42228e6f92fa98e2414c56f63c65abd786b021c
ZiplEix <ZiplEix@users.noreply.github.com>
2024-11-13 19:06:32 +0900

    feat: add loader to button

--- web/src/routes/dashboard/+page.svelte
+++ web/src/routes/dashboard/+page.svelte
@@ -1,11 +1,37 @@
 <script lang='ts'>
+  import Loader from "$lib/ui/Loader.svelte";
+import { writable } from "svelte/store";
+
     let gamecode = '';
+
+    let createLoading = writable(false);
+    let joinLoading = writable(false);
+
+    function createGame() {
+        createLoading.set(true);
+        setTimeout(() => {
+            createLoading.set(false);
+        }, 2000);
+    }
+
+    function joinGame() {
+        joinLoading.set(true);
+        setTimeout(() => {
+            joinLoading.set(false);
+        }, 2000);
+    }
 </script>
 
 <div class="grid h-96 place-items-center">
     <div class="flex flex-col items-center space-y-4">
         <h1 class="text-5xl font-bold mb-4">Dashboard</h1>
-        <button class="btn w-full">CREATE GAME</button>
+        <button class="btn w-full" on:click={createGame}>
+            {#if $createLoading}
+                <Loader />
+            {:else}
+                CREATE GAME
+            {/if}
+        </button>
         <div class="divider">OR</div>
         <div class="flex flex-row gap-4">
             <input
@@ -17,7 +43,13 @@
                 bind:value={gamecode}
                 required
             />
-            <button class="btn">JOIN GAME</button>
+            <button class="btn" on:click={joinGame}>
+                {#if $joinLoading}
+                    <Loader />
+                {:else}
+                    JOIN GAME
+                {/if}
+            </button>
         </div>
     </div>
 </div>
