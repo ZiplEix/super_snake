4cb8f0a67829e6ec61e769c4b3a7402eefa997d2
ZiplEix <ZiplEix@users.noreply.github.com>
2024-11-13 18:34:18 +0900

    feat: add dashboard and auth verification

---
+++ web/src/lib/ui/NavBar.svelte
@@ -0,0 +1,29 @@
+<script lang='ts'>
+    import { logout } from "$lib";
+</script>
+
+<nav class="navbar bg-base-200">
+    <div class="flex-1">
+        <a href="/dashboard" class="btn btn-ghost text-xl">SuperSnake</a>
+    </div>
+    <div class="flex-none">
+        <div class="dropdown dropdown-end">
+        </div>
+        <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                    <img
+                        alt="Tailwind CSS Navbar component"
+                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                    />
+                </div>
+            </div>
+            <ul
+                class="menu menu-sm dropdown-content bg-base-200 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+                <li><a href="/dashboard">Settings</a></li>
+                <li><button on:click={logout}>Logout</button></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ web/src/routes/dashboard/+layout.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang='ts'>
+    export async function load({ url }: { url: URL }) {
+        return {
+            queryParam: url.searchParams.get('redirectTo') || '/'
+        }
+    }
+</script>
+
+<script lang='ts'>
+  import NavBar from "$lib/ui/NavBar.svelte";
+
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        const jwt = localStorage.getItem('jwt');
+
+        if (!jwt) {
+            window.location.href = 'login';
+        }
+    });
+</script>
+
+<NavBar />
+
+<slot />
