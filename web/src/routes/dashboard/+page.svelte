37400b2f4dc59850d7021f952ca8734bd2927fe2
ZiplEix <ZiplEix@users.noreply.github.com>
2024-11-13 18:44:24 +0900

    feat: add create and join game ui in dashboard

--- web/src/routes/dashboard/+page.svelte
+++ web/src/routes/dashboard/+page.svelte
@@ -1,5 +1,22 @@
 <script lang='ts'>
-    
+    let gamecode = '';
 </script>
 
-<h1>SALUT</h1>
+<div class="grid h-screen place-items-center">
+    <div class="flex flex-col items-center space-y-4">
+        <h1 class="text-5xl font-bold mb-4">Dashboard</h1>
+        <button class="btn w-full">CREATE GAME</button>
+        <div class="flex flex-row gap-4">
+            <input
+                type="email"
+                placeholder="XXXXXX"
+                class="input input-bordered input-primary w-full"
+                id="email"
+                name="email"
+                bind:value={gamecode}
+                required
+            />
+            <button class="btn">JOIN GAME</button>
+        </div>
+    </div>
+</div>
