5ddd17057d0a94e41f8b85506b12e0a894b6bbec
ZiplEix <ZiplEix@users.noreply.github.com>
2024-11-16 14:55:07 +0900

    feat: update landing page with new layout and navigation buttons

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,4 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<a href="/snake" class="btn btn-primary">GO TO SNAKE</a>
+<div class="grid h-screen place-items-center">
+    <div class="flex flex-col items-center space-y-16">
+        <h1 class="text-5xl font-bold">SUPER SNAKE</h1>
+        <div class="flex gap-4">
+            <a href="/login" class="btn btn-primary">Login</a>
+            <a href="/register" class="btn btn-primary">Register</a>
+        </div>
+    </div>
+</div>
